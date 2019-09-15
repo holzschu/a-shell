7386b5d522589f1b8ef8ddec22164727a89117a5
HOLZSCHUCH Nicolas <nicolas.holzschuch@inria.fr>
2019-09-15 18:39:38 +0200

    New dictionaries with TeX commands, to be loaded on demand

--- WkWebViewExtension.swift
+++ WkWebViewExtension.swift
@@ -63,44 +63,36 @@ extension WKWebView {
     // Second extension, keyboardDisplayRequiresUserAction
     // solution from https://stackoverflow.com/questions/32449870/programmatically-focus-on-a-form-in-a-webview-wkwebview/48623286#48623286
     // First solution from @alexstaravoitau
-    var keyboardDisplayRequiresUserAction: Bool? {
-        get {
-            return self.keyboardDisplayRequiresUserAction
-        }
-        set {
-            self.setKeyboardRequiresUserInteraction(newValue ?? true)
-        }
-    }
-    
-    private var webContentView: UIView? {
-        for subview in (self.scrollView.subviews) {
-            if subview.classForCoder.description() == "WKContentView" {
-                return subview
-            }
-            // adding the toolbar has changed the name of the view:
-            if subview.classForCoder.description() == "WKApplicationStateTrackingView_CustomInputAccessoryView" {
-                return subview
-            }
-        }
-        return nil
-    }
-    
-    func setKeyboardRequiresUserInteraction( _ value: Bool) {
+    func allowDisplayingKeyboardWithoutUserAction() {
         guard let WKContentView: AnyClass = NSClassFromString("WKContentView") else {
-            print("keyboardDisplayRequiresUserAction extension: Cannot find the WKContentView class")
+            print("allowDisplayingKeyboardWithoutUserAction extension: Cannot find the WKContentView class")
             return
         }
-        // For iOS 13.*
-        let selector: Selector = sel_getUid("_elementDidFocus:userIsInteracting:blurPreviousNode:activityStateChanges:userObject:")
-        
+        var selector: Selector = sel_getUid("_elementDidFocus:userIsInteracting:blurPreviousNode:activityStateChanges:userObject:")
         if let method = class_getInstanceMethod(WKContentView, selector) {
             let originalImp: IMP = method_getImplementation(method)
             let original: NewClosureType = unsafeBitCast(originalImp, to: NewClosureType.self)
             let block : @convention(block) (Any, UnsafeRawPointer, Bool, Bool, Bool, Any?) -> Void = { (me, arg0, arg1, arg2, arg3, arg4) in
-                original(me, selector, arg0, !value, arg2, arg3, arg4)
+                original(me, selector, arg0, true, arg2, arg3, arg4)
             }
-            let imp: IMP = imp_implementationWithBlock(block)
-            method_setImplementation(method, imp)
+            let override: IMP = imp_implementationWithBlock(block)
+
+            method_setImplementation(method, override);
+        }
+        guard let WKContentViewAgain: AnyClass = NSClassFromString("WKApplicationStateTrackingView_CustomInputAccessoryView") else {
+            print("allowDisplayingKeyboardWithoutUserAction extension: Cannot find the WKApplicationStateTrackingView_CustomInputAccessoryView class")
+            return
+        }
+        selector = sel_getUid("_elementDidFocus:userIsInteracting:blurPreviousNode:activityStateChanges:userObject:")
+        if let method = class_getInstanceMethod(WKContentViewAgain, selector) {
+            let originalImp: IMP = method_getImplementation(method)
+            let original: NewClosureType = unsafeBitCast(originalImp, to: NewClosureType.self)
+            let block : @convention(block) (Any, UnsafeRawPointer, Bool, Bool, Bool, Any?) -> Void = { (me, arg0, arg1, arg2, arg3, arg4) in
+                original(me, selector, arg0, true, arg2, arg3, arg4)
+            }
+            let override: IMP = imp_implementationWithBlock(block)
+            
+            method_setImplementation(method, override);
         }
     }
     
