5191b68052a2d48e2f25c887bf7a2ec0ef594c9d
HOLZSCHUCH Nicolas <nicolas.holzschuch@inria.fr>
2022-12-14 20:39:28 +0100

    Code for customizing the keyboard toolbar

--- Util/String+C.swift
+++ Util/String+C.swift
@@ -7,6 +7,7 @@
 //
 
 import Foundation
+import UIKit
 
 extension String {
 	
@@ -18,7 +19,29 @@ extension String {
 	var utf8CString: UnsafeMutablePointer<Int8> {
 		return UnsafeMutablePointer(mutating: (self as NSString).utf8String!)
 	}
-	
+    
+    /// Generates a `UIImage` instance from this string using a specified
+    /// attributes and size.
+    ///
+    /// - Parameters:
+    ///     - attributes: to draw this string with. Default is `nil`.
+    ///     - size: of the image to return.
+    /// - Returns: a `UIImage` instance from this string using a specified
+    /// attributes and size, or `nil` if the operation fails.
+    /// https://stackoverflow.com/questions/51100121/how-to-generate-an-uiimage-from-custom-text-in-swift
+    func image(withAttributes attributes: [NSAttributedString.Key: Any]? = nil, size: CGSize? = nil) -> UIImage? {
+        let textSize = (self as NSString).size(withAttributes: attributes)
+        var size = size ?? textSize
+        if (textSize.width > size.width) {
+            size.width = textSize.width
+        }
+        let origin = CGPoint(x:size.width/2 - textSize.width/2, y: size.height/2 - textSize.height/2)
+        return UIGraphicsImageRenderer(size: size).image { (context) in
+            (self as NSString).draw(in: CGRect(origin: origin, size: size),
+                                    withAttributes: attributes)
+        }
+    }
+    	
 }
 
 func convertCArguments(argc: Int32, argv: UnsafeMutablePointer<UnsafeMutablePointer<Int8>?>?) -> [String]? {
