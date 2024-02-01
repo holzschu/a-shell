296bdd205f2b1f0f64eda39128e00acb604e86a3
HOLZSCHUCH Nicolas <nicolas.holzschuch@inria.fr>
2024-02-01 14:49:12 +0100

    Update libssh2 / openssl to 1.11.0 / 1.1.1w

--- xcfs/Package.swift
+++ xcfs/Package.swift
@@ -5,20 +5,20 @@ _ = Package(
     name: "xcfs",
     platforms: [.macOS("11")],
     dependencies: [
-        .package(url: "https://github.com/yury/FMake", from: "0.0.16")
+        .package(url: "https://github.com/holzschu/FMake", from: "0.0.19")
     ],
     
     targets: [
         // libssh2
         .binaryTarget(
             name: "libssh2",
-            url: "https://github.com/blinksh/libssh2-apple/releases/download/v1.9.0/libssh2-dynamic.xcframework.zip",
-            checksum: "a4b5d09cd799b32a019148168983499e8501d93c2a458a9cc547b03028d96040"
+            url: "https://github.com/holzschu/libssh2-apple/releases/download/v1.11.0/libssh2-dynamic.xcframework.zip",
+            checksum: "cacfe1789b197b727119f7e32f561eaf9acc27bf38cd19975b74fce107f868a6"
         ),
         .binaryTarget(
             name: "openssl",
-            url: "https://github.com/blinksh/openssl-apple/releases/download/v1.1.1k/openssl-dynamic.xcframework.zip",
-            checksum: "9a7cc2686122e62445b85a8ce04f49379d99c952b8ea3534127c004b8a00af59"
+            url: "https://github.com/holzschu/openssl-apple/releases/download/v1.1.1w/openssl-dynamic.xcframework.zip",
+            checksum: "421712e7150240d57294897bf3e2b0782246d7c687a4a82c3cd053d70a5f08b9"
         ),
         .binaryTarget(
             name: "ios_system",
